db21fc2d528f4cc7ef587ce478120a5c05a54faf
Francisco Ferraz <ffranciscofferraz@gmail.com>
2023-08-02 20:25:54 +0100

    CORS

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -38,6 +39,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -84,6 +88,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", smtp_username, "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", smtp_password, "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "openMovie <no-reply@openmovie.ffranciscofferraz.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
